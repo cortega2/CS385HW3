0a2da627fccbe69eb74025162a8afa88f2b771e6
Carlos Ortega <carlos@reptar.(none)>
2013-11-16 18:10:45 -0600

    done with calculations need to do branch and bound and visualize!!!

--- orderSearcher.c
+++ orderSearcher.c
@@ -3,9 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <math.h>
 
+#define SEMNUM 7
+
+//globals
 void * thread_function(void * arg);
-char message[] = "hello";
+sem_t sems[7];
+
+unsigned int highestAvg = 0;
+unsigned int lowestAvg = 255;
+unsigned int maxChange = 0;
+unsigned long maxSumChange = 0;
+double lowStdDev = 255.0;
+double lowStdChangeDev = 255.0;
+int range[2] = {255, 0};
+
 
 struct threadArgs{
 	int id;
@@ -15,16 +29,18 @@ struct threadArgs{
 
 int main(int argc, char * argv[]){
 	//simple error check
+	printf("NAME: Carlos Ortega  NETID: corteg20\n");
+
 	if(argc <=2){
 		printf("Not enought arguments\n");
 		return -1;
 	}
 
-	//Read file
+	//file handling
 	int numThreads = atoi(argv[2]);
 	FILE *file;
 	long flength;
-	char *fileData;
+	unsigned char *fileData;
 
 	file = fopen(argv[1], "rb");	
 	if(!file){
@@ -40,22 +56,24 @@ int main(int argc, char * argv[]){
 	fileData = (char *) malloc(flength+1);
 	fread(fileData, flength, 1, file);
 	fclose(file);
+	//end of file handing
+
 
 	//create 2darray that will be used by the threads
-
-	int *intData[numThreads]; 
+	unsigned int *intData[numThreads]; 
 	int x;
 	for(x=0; x<numThreads; x++){
+		//if not evenly divisible
 		if(x== numThreads - 1)
-			intData[x] = (int *) malloc((flength/numThreads + flength%numThreads)*sizeof(int));			//if not evenly divisible
+			intData[x] = (unsigned int *) malloc((flength/numThreads + flength%numThreads)*sizeof(unsigned int));
 		else
-			intData[x] = (int *) malloc((flength/numThreads)*sizeof(int));
+			intData[x] = (unsigned int *) malloc((flength/numThreads)*sizeof(unsigned int));
 	}
     
-	//populate int 2darray with values from the file which are in the fileData array and set pointer to the array in the struct
-	//that will be passed to each thread
+	//populate int 2darray with values from the file which are in the fileData array and set
+	//pointer to the array in the struct that will be passed to each thread
 	struct threadArgs toTheThreads[numThreads];
-	int counter = 0;
+	int counter = 0	;
 	for(x=0; x<numThreads; x++){
 		long limit;
 		long y;
@@ -77,6 +95,15 @@ int main(int argc, char * argv[]){
 	pthread_t many_threads[numThreads];
 	void * thread_result;
 
+	//initialize semaphores
+	for(x = 0; x < SEMNUM; x++ ){
+		res = sem_init(&sems[x], 0, 1);
+		if(res != 0){
+			perror("semaphore creation error");
+			exit(EXIT_FAILURE);
+		}
+	}
+
 	//Create threads
 	int temp =3;
 	for(x=0; x<numThreads; x++){
@@ -87,6 +114,8 @@ int main(int argc, char * argv[]){
 			exit(EXIT_FAILURE);
 		}
 	}	
+
+	//wait for threads
 	printf("Waiting for the thread to finish\n");
 	for(x=numThreads-1; x >= 0; x--){
 
@@ -96,24 +125,115 @@ int main(int argc, char * argv[]){
 			exit(EXIT_FAILURE);
 		}
 	}
-	
-	//printf("Thread joined, it returned %s\n", (char *)thread_result);
+
+	printf("Highest Average: %d\n", highestAvg);	
+	printf("Lowest Average: %d\n", lowestAvg);
+	printf("Highest Range: %d\nLowest Range: %d\n", range[1], range[0]);
+	printf("Max change: %d\n", maxChange);
+	printf("Max Sum Change: %d\n", maxSumChange);
+	printf("Min Standard Dev: %f\n", lowStdDev);
+	printf("Min Std Dev of Data Change: %f\n", lowStdChangeDev);
 	printf("Done! \n");
+
+	for(x = 0; x < SEMNUM; x++)
+		sem_destroy(&sems[x]);
+
 	exit(EXIT_SUCCESS);
 }
 
 void * thread_function(void * arg){
 	struct threadArgs vars = *(struct threadArgs *) arg;
 	
-	//calculate average
+	//calculate average and range and change
 	long size = vars.dataSize;
 	long average = 0;
 	long x;
+	int lowest = 255;
+	int highest = 0;
+	int Localrange = 0;
+	int change = 0;
+	int changeSum = 0;
+	double LocalStdDev = 0;
+	int diff = 0;
+		
 	for(x = 0; x<size; x++){
+		if(vars.data[x] < lowest)
+			lowest = vars.data[x];
+		if(vars.data[x] > highest)
+			highest = vars.data[x];
 		average = average + vars.data[x];
+
+		if(x!= size - 1){
+			int absolute = abs(vars.data[x] - vars.data[x+1]);
+			changeSum = changeSum + absolute;
+			if( absolute > change)
+				change = absolute;
+		}
 	}
 	average = average/size;
-	printf("Thread ID[%d] Average: %ld\n", vars.id, average);
+	Localrange = highest - lowest;
+
+	//update highest average
+	sem_wait(&sems[0]);
+	if(average > highestAvg)
+	highestAvg = average;
+	sem_post(&sems[0]);
+
+	//update lowest average
+	sem_wait(&sems[1]);
+	if(average < lowestAvg)
+		lowestAvg = average;
+	sem_post(&sems[1]);
+
+	//update ranges
+	sem_wait(&sems[2]);
+	if(Localrange < range[0])
+		range[0] = Localrange;
+	if(Localrange > range[1])
+		range[1] = Localrange;
+	sem_post(&sems[2]);
+
+	//update max changes
+	sem_wait(&sems[3]);
+	if(change > maxChange)
+		maxChange = change;
+	sem_post(&sems[3]);
+
+	//update maxSumChange
+	sem_wait(&sems[4]);
+	if(changeSum > maxSumChange)
+		maxSumChange = changeSum;
+	sem_post(&sems[4]);
+
+	//find std dev
+	for(x = 0; x < size; x++)
+		diff = diff + (vars.data[x] - average) * (vars.data[x] - average);
+
+	LocalStdDev = sqrt((double)diff/size);
+	
+	sem_wait(&sems[5]);
+	if(LocalStdDev < lowStdDev)
+		lowStdDev = LocalStdDev;
+	sem_post(&sems[5]);
+
+	//find std dev of change of data
+	double LocalStdChangeDev = 0;
+	double changeDiff = 0.0;
+	double absSumAvg = 0.0;
+	for(x =0; x < size-1; x ++)
+		absSumAvg = absSumAvg + abs(vars.data[x] - vars.data[x+1]);
+	absSumAvg = (double)absSumAvg / (size-1);
+
+	for(x = 0; x < size -1 ;x++){
+		int absolute = abs(vars.data[x] - vars.data[x+1]);
+		changeDiff = changeDiff + (absolute - absSumAvg) * (absolute- absSumAvg);
+	}
+	LocalStdChangeDev = sqrt(changeDiff / (size-1));
+	sem_wait(&sems[6]);
+	if(LocalStdChangeDev < lowStdChangeDev)
+		lowStdChangeDev = LocalStdChangeDev;
+	sem_post(&sems[6]);
+
 
 	pthread_exit("Thank you for the cpu time");
 }
